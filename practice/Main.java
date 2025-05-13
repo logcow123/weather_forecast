bb358ecf1639da1b88c8c779f7724aa8782660c9
logcow123 <logcow123@gmail.com>
2025-05-13 15:14:53 -0600

    Set up enviroment

---
+++ practice/Main.java
@@ -0,0 +1,14 @@
+package practice;
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Variables
+        String name = "Logan";
+        Integer num = 5;
+        System.out.println(name + " " + (num + num));
+
+    }
+
+}
